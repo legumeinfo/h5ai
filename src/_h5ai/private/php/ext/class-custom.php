57efeeb272aef4745e78a645d7bbdf6843a976f0
Nathan Weeks <1800812+nathanweeks@users.noreply.github.com>
2022-05-10 07:50:00 -0500

    Append README.yml in footer
    Hard-code HTML portion of footer in source

--- src/_h5ai/private/php/ext/class-custom.php
+++ src/_h5ai/private/php/ext/class-custom.php
@@ -1,24 +1,10 @@
 <?php
 
 class Custom {
-    private static $EXTENSIONS = ['html', 'md'];
     private $context;
 
     public function __construct($context) {
         $this->context = $context;
-    }
-
-    private function read_custom_file($path, $name, &$content, &$type) {
-        $file_prefix = $this->context->get_setup()->get('FILE_PREFIX');
-
-        foreach (Custom::$EXTENSIONS as $ext) {
-            $file = $path . '/' . $file_prefix . '.' . $name . '.' . $ext;
-            if (is_readable($file)) {
-                $content = file_get_contents($file);
-                $type = $ext;
-                return;
-            }
-        }
     }
 
     public function get_customizations($href) {
@@ -29,16 +15,37 @@ class Custom {
             ];
         }
 
-        $root_path = $this->context->get_setup()->get('FILE_PREFIX');
         $path = $this->context->to_path($href);
 
         $header = null;
         $header_type = null;
-        $footer = null;
-        $footer_type = null;
-
-        $this->read_custom_file($path, 'header', $header, $header_type);
-        $this->read_custom_file($path, 'footer', $footer, $footer_type);
+        $footer_type = 'html';
+        $footer ='
+<p>
+<b>Abbreviations</b>:
+<b>gnm</b> = genome assembly; 
+<b>ann</b> = annotation; 
+<b>bac</b> = BAC library;   
+<b>map</b> = map; 
+<b>mrk</b> = markers; 
+<b>div</b> = diversity; 
+<b>gws</b> = GWAS; 
+<b>fam</b> = gene family;   
+<b>rpt</b> = repeats; 
+<b>tcp</b> = transcriptome;
+<b>trt</b> = traits;
+<b>syn</b> = synteny; 
+<b>.gzi, .fai</b> = index files produced by samtools fadix
+</p> <br>
+<p><b>Navigating</b>: Navigate via the folder links below or to the left.</p>
+<p><b>Downloading</b>: Mark one or more folders or files with a checkmark (hover to the left of the folder icon), then click the download icon (down-arrow in the upper left).</p>
+<p><b>Searching</b>: See the magnifying glass in the upper left.</p>
+<p><b>Dataset key names and Registry</b>: The four-letter string in the README and the filenames (e.g. gNmT) is a unique key, which associates the file(s) in a directory (a data collection) with the metadata for the file(s). The keys are also recorded at the <a href="http://bit.ly/LegFed_registry">Registry</a>.</p>
+<br>
+<p><b>Please <a href="/contact">contact us</a></b> to contribute data, or to set up a similar repository. 
+Metadata templates and protocols: see the <a href="/data/metadata/">metadata directory</a> and 
+<a href="https://github.com/LegumeFederation/datastore/">github</a>.</p>
+<hr>';
 
         if ($header === null) {
             foreach (glob($path . "/*/README.*.yml") as $readme_path) {
@@ -49,29 +56,12 @@ class Custom {
             }
         }
 
-        while ($header === null || $footer === null) {
-            if ($header === null) {
-                $this->read_custom_file($path, 'headers', $header, $header_type);
-            }
-            if ($footer === null) {
-                $this->read_custom_file($path, 'footers', $footer, $footer_type);
-            }
-            if ($path === $root_path) {
+        foreach (glob($path . "/README.*.yml") as $readme_path) {
+            if (is_readable($readme_path)) {
+                $content = file_get_contents($readme_path);
+                $footer .= '<pre>' . file_get_contents($readme_path) . '</pre>';
                 break;
             }
-            $parent_path = Util::normalize_path(dirname($path));
-            if ($parent_path === $path) {
-                break;
-            }
-
-            // Stop once we reach the root
-            if (
-                $this->context->query_option('custom.stopSearchingAtRoot', true) &&
-                $path === $this->context->get_setup()->get('ROOT_PATH')
-            ) {
-                break;
-            }
-            $path = $parent_path;
         }
 
         return [

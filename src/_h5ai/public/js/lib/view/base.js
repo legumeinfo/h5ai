1ac4b15d9c98ac55fbcd4672912fc6e3ea91d651
Chen Prom <chen@arsiaamc23chenlt.local>
2024-08-27 08:32:14 -0500

    update

--- src/_h5ai/public/js/lib/view/base.js
+++ src/_h5ai/public/js/lib/view/base.js
@@ -1,10 +1,11 @@
 const {dom} = require('../util');
 
 const SEL_ROOT = 'body';
+let location = window.location.hostname.includes('soybase.org');
 const TPL_TOPBAR =
-        `<div id="topbar">
-        <a href="javascript: location.hostname.includes(\`soybase.org\`) ? window.location = \`https://www.soybase.org/\` : window.location = \`https://www.legumeinfo.org\`">
-        <img src="https://www.legumeinfo.org/assets/img/lis-logo-small.png" alt="Legume Information System logo">
+       location ? `<div id="topbar">
+        <a href="javascript: window.location = \`https://www.soybase.org/\` ">
+       <img src="https://dev.soybase.org/assets/img/sb_logo.png" height=51 width=50 alt="Soybase logo">
         </a>
             <div id="toolbar"></div>
             <div id="flowbar"></div>
@@ -14,7 +15,21 @@ const TPL_TOPBAR =
                 <div>by h5ai</div>
             </a>
             -->
-        </div>`;
+        </div>` 
+        : 
+        `<div id="topbar">
+        <a href="javascript: window.location = \`https://www.legumeinfo.org/\`">
+       <img src="https://www.legumeinfo.org/assets/img/lis-logo-small.png" alt="Legume Information System logo">
+        </a>
+            <div id="toolbar"></div>
+            <div id="flowbar"></div>
+            <!--
+            <a id="backlink" href="https://larsjung.de/h5ai/" title="powered by h5ai - https://larsjung.de/h5ai/">
+                <div>powered</div>
+                <div>by h5ai</div>
+            </a>
+            -->
+        </div>`
 const TPL_MAINROW =
         `<div id="mainrow">
             <div id="content"></div>
